41d6c88e9ce4ced67ef51ffd3b6d667d3c533fc0
Xx <8x.4y.2z@gmail.com>
2022-11-15 00:05:21 +0530

    Get detectron directly

--- src/core/roi_heads/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .roi_heads import build_roi_heads
-
-
-__all__ = [
-    "build_roi_heads"
-]
--- src/engine/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .defaults import default_arg_parser, DefaultTrainer, default_setup
-
-__all__ = ["default_arg_parser","DefaultTrainer"]
