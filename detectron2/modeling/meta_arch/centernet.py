3636eea429b6a9f1b90341cd05601160561950e4
Xx <8x.4y.2z@gmail.com>
2022-11-22 17:20:52 +0530

    Add centernet scripts

---
+++ detectron2/modeling/meta_arch/centernet.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+from typing import Tuple, Optional, List, Dict
+
+import torch
+from torch import nn
+
+from detectron2.config.config import configurable
+from detectron2.modeling.backbone import Backbone, build_backbone
+from detectron2.modeling.proposal_generator import build_proposal_generator
+from detectron2.layers import move_device_like
+from detectron2.utils.events import get_event_storage
+from .build import META_ARCH_REGISTRY
+
+
+
+@META_ARCH_REGISTRY.register()
+class Centernet(nn.Module):
+
+    @configurable
+    def __init__(self,*,backbone: Backbone,
+                 proposal_generator: nn.Module,
+                 pixel_mean: Tuple[float],
+                 pixel_std: Tuple[float],
+                 input_format: Optional[str] = None,
+                 vis_period: int = 0,
+                 ):
+        super(Centernet, self).__init__()
+        self.backbone = backbone,
+        self.proposal_generator = proposal_generator
+        self.input_format = input_format
+        self.vis_period = vis_period
+        if vis_period > 0:
+            assert input_format is not None, "input_format is required for visualization!"
+
+        self.register_buffer("pixel_mean", torch.tensor(pixel_mean).view(-1, 1, 1), False)
+        self.register_buffer("pixel_std", torch.tensor(pixel_std).view(-1, 1, 1), False)
+        assert (
+            self.pixel_mean.shape == self.pixel_std.shape
+        ), f"{self.pixel_mean} and {self.pixel_std} have different shapes!"
+
+
+
+    @classmethod
+    def from_config(cls, cfg):
+        backbone = build_backbone(cfg)
+        return {
+            "backbone": backbone,
+            "proposal_generator": build_proposal_generator(cfg, backbone.output_shape()),
+            "input_format": cfg.INPUT.FORMAT,
+            "vis_period": cfg.VIS_PERIOD,
+            "pixel_mean": cfg.MODEL.PIXEL_MEAN,
+            "pixel_std": cfg.MODEL.PIXEL_STD,
+        }
+
+
+    @property
+    def device(self):
+        return self.pixel_mean.device
+
+    def _move_to_current_device(self, x):
+        return move_device_like(x, self.pixel_mean)
+
+
+    def forward(self, batched_inputs: List[Dict[str, torch.Tensor]]):
+        """
+        Args:
+            batched_inputs: a list, batched outputs of :class:`DatasetMapper` .
+                Each item in the list contains the inputs for one image.
+                For now, each item in the list is a dict that contains:
+
+                * image: Tensor, image in (C, H, W) format.
+                * instances (optional): groundtruth :class:`Instances`
+                * proposals (optional): :class:`Instances`, precomputed proposals.
+
+                Other information that's included in the original dicts, such as:
+
+                * "height", "width" (int): the output resolution of the model, used in inference.
+                  See :meth:`postprocess` for details.
+
+        Returns:
+            list[dict]:
+                Each dict is the output for one input image.
+                The dict contains one key "instances" whose value is a :class:`Instances`.
+                The :class:`Instances` object has the following keys:
+                "pred_boxes", "pred_classes", "scores", "pred_masks", "pred_keypoints"
+        """
+        if not self.training:
+            return self.inference(batched_inputs)
+
+        images = self.preprocess_image(batched_inputs)
+        if "instances" in batched_inputs[0]:
+            gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
+        else:
+            gt_instances = None
+
+        features = self.backbone(images.tensor)
+
+        proposals, proposal_losses = self.proposal_generator(images, features, gt_instances)
+
+        if self.vis_period > 0:
+            storage = get_event_storage()
+            if storage.iter % self.vis_period == 0:
+                self.visualize_training(batched_inputs, proposals)
+
+        losses = {}
+        losses.update(proposal_losses)
+        return losses
+
+    def inference(
+        self,
+        batched_inputs: List[Dict[str, torch.Tensor]],
+        detected_instances: Optional[List[Instances]] = None,
+        do_postprocess: bool = True,
+    ):
+        """
+        Run inference on the given inputs.
+
+        Args:
+            batched_inputs (list[dict]): same as in :meth:`forward`
+            detected_instances (None or list[Instances]): if not None, it
+                contains an `Instances` object per image. The `Instances`
+                object contains "pred_boxes" and "pred_classes" which are
+                known boxes in the image.
+                The inference will then skip the detection of bounding boxes,
+                and only predict other per-ROI outputs.
+            do_postprocess (bool): whether to apply post-processing on the outputs.
+
+        Returns:
+            When do_postprocess=True, same as in :meth:`forward`.
+            Otherwise, a list[Instances] containing raw network outputs.
+        """
+        assert not self.training
+
+        images = self.preprocess_image(batched_inputs)
+        features = self.backbone(images.tensor)
+
+        if detected_instances is None:
+            if self.proposal_generator is not None:
+                proposals, _ = self.proposal_generator(images, features, None)
+            else:
+                assert "proposals" in batched_inputs[0]
+                proposals = [x["proposals"].to(self.device) for x in batched_inputs]
+
+            results, _ = self.roi_heads(images, features, proposals, None)
+        else:
+            detected_instances = [x.to(self.device) for x in detected_instances]
+            results = self.roi_heads.forward_with_given_boxes(features, detected_instances)
+
+        if do_postprocess:
+            assert not torch.jit.is_scripting(), "Scripting is not supported for postprocess."
+            return Centernet._postprocess(results, batched_inputs, images.image_sizes)
+        return results
+
+
+    def preprocess_image(self, batched_inputs: List[Dict[str, torch.Tensor]]):
+        """
+        Normalize, pad and batch the input images.
+        """
+        images = [self._move_to_current_device(x["image"]) for x in batched_inputs]
+        images = [(x - self.pixel_mean) / self.pixel_std for x in images]
+        images = ImageList.from_tensors(
+            images,
+            self.backbone.size_divisibility,
+            padding_constraints=self.backbone.padding_constraints,
+        )
+        return images
+
+    @staticmethod
+    def _postprocess(instances, batched_inputs: List[Dict[str, torch.Tensor]], image_sizes):
+        """
+        Rescale the output instances to the target size.
+        """
+        # note: private function; subject to changes
+        processed_results = []
+        for results_per_image, input_per_image, image_size in zip(
+            instances, batched_inputs, image_sizes
+        ):
+            height = input_per_image.get("height", image_size[0])
+            width = input_per_image.get("width", image_size[1])
+            r = detector_postprocess(results_per_image, height, width)
+            processed_results.append({"instances": r})
+        return processed_results
---
+++ detectron2/modeling/proposal_generator/centernet_heads.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch import nn
+
+from detectron2.layers.wrappers import Conv2d
+from detectron2.config.config import configurable
+
+
+class CenternetHeads(nn.Module):
+
+
+    @configurable
+    def __init__(self,in_channels, nheads_in, hm_heads,rg_heads,wh_heads):
+        self.heatmap_layer = nn.Sequential(
+            Conv2d(in_channels,nheads_in,kernel_size=3,stride=1,padding="same"),
+            nn.ReLU(),
+            Conv2d(nheads_in,hm_heads,kernel_size=1,stride=1,padding="same")
+        )
+
+        self.reg_layer = nn.Sequential(
+            Conv2d(in_channels,nheads_in, kernel_size=(3, 3), strides=1, padding="same"),
+            nn.ReLU(),
+            Conv2d(nheads_in,rg_heads, kernel_size=(1, 1), strides=1, padding="same")
+        )
+        self.wh_layer = nn.Sequential(
+            Conv2d(in_channels,nheads_in, kernel_size=(3, 3), strides=1, padding="same"),
+            nn.ReLU(),
+            Conv2d(nheads_in,wh_heads, kernel_size=(1, 1), strides=1, padding="same")
+        )
+
+
+    def forward(self,x):
+        heatmap = self.heatmap_layer(x)
+        reg = self.reg_layer(x)
+        wh = self.wh_layer(x)
+
+        return [heatmap, reg, wh]
+
+
+    @classmethod
+    def from_config(cls, cfg):
+        return {
+            "in_channels":None,
+            "nheads_in":None,
+            "hm_heads":None,
+            "rg_heads":None,
+            "wh_heads":None,
+        }
