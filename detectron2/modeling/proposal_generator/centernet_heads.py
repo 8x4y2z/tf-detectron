49ed0f24d3c52e3d571599035d2c98095c478b1f
Pupil <8x.4y.2z@gmail.com>
2022-11-27 12:24:30 +0530

    Add gradient clipping

--- detectron2/modeling/proposal_generator/centernet_heads.py
+++ detectron2/modeling/proposal_generator/centernet_heads.py
@@ -21,7 +21,7 @@ class CenternetHeads(nn.Module):
         self.bnlayers = nn.Sequential(
            *[BatchNorm2d(in_channels) for _ in range(nlayers-1)]
         )
-        for _ in range(nlayers-1):
+        for _ in range(nlayers-2):
             self.transpose_layers.append(
                     nn.ConvTranspose2d(
                         in_channels,
@@ -54,13 +54,38 @@ class CenternetHeads(nn.Module):
 
 
     def forward(self,x,dims):
-        ps = ("p6","p5","p4","p3","p2")
-        for i,p in enumerate(ps):
-            if i < 4:
-                out = self.transpose_layers[i](x[p])
-                out = self.bnlayers[i](out)
-                x[ps[i+1]],out = self._adjust_dim(x[ps[i+1]], out)
-                x[ps[i+1]] += out
+        # out = self.transpose_layers[0](x["p6"])
+        # out = self.bnlayers[0](out)
+        # out = F.interpolate(out,(25,34))
+        # x["p5"] += out
+
+        out = self.transpose_layers[0](x["p5"],output_size = x["p4"].size())
+        out = self.bnlayers[0](out)
+        # out = F.interpolate(out,(50,68))
+        x["p4"] += out
+
+        out = self.transpose_layers[1](x["p4"],output_size = x["p3"].size())
+        out = self.bnlayers[1](out)
+        # out = F.interpolate(out,(100,136))
+        x["p3"] += out
+
+        out = self.transpose_layers[2](x["p3"],output_size = x["p2"].size())
+        out = self.bnlayers[2](out)
+        # out = F.interpolate(out,(200,272))
+        x["p2"] += out
+
+        # out = self.transpose_layers[0](x["p6"])
+        # out = self.bnlayers[0](out)
+        # out = F.interpolate(out,(25,34))
+        # x["p5"] += out
+
+        # ps = ("p6","p5","p4","p3","p2")
+        # for i,p in enumerate(ps):
+        #     if i < 4:
+        #         out = self.transpose_layers[i](x[p])
+        #         out = self.bnlayers[i](out)
+        #         out = F.interpolate(out,x[ps[i+1]].shape[-2:])
+        #         x[ps[i+1]] += out
 
         x["p2"] = F.interpolate(x["p2"],dims)
 
@@ -69,46 +94,46 @@ class CenternetHeads(nn.Module):
         wh = self.wh_layer(x["p2"])
         return torch.cat([heatmap, reg, wh],1)
 
-    @torch.no_grad()
-    def _adjust_dim(self,t1:torch.Tensor,t2:torch.Tensor):
-        r_t1,c_t1 = t1.shape[-2:]
-        r_t2,c_t2 = t2.shape[-2:]
-        max_row = max(r_t1,r_t2) if r_t1 != r_t2 else None
-        max_col = max(c_t1,c_t2) if c_t1 != c_t2 else None
+    # @torch.no_grad()
+    # def _adjust_dim(self,t1:torch.Tensor,t2:torch.Tensor):
+    #     r_t1,c_t1 = t1.shape[-2:]
+    #     r_t2,c_t2 = t2.shape[-2:]
+    #     max_row = max(r_t1,r_t2) if r_t1 != r_t2 else None
+    #     max_col = max(c_t1,c_t2) if c_t1 != c_t2 else None
 
-        if max_row is not None:
-            to_add = t2 if max_row == r_t1 else t1
-            while max_row - min(r_t1,r_t2)> 0:
-                to_add = torch.cat(
-                    (
-                        to_add,
-                     torch.zeros((*to_add.shape[:2],1,to_add.shape[-1]),device=to_add.device)
-                    ),
-                    -2
-                )
-                max_row -= 1
-            if r_t1 > r_t2:
-                t2 = to_add
-            else:
-                t1 = to_add
+    #     if max_row is not None:
+    #         to_add = t2 if max_row == r_t1 else t1
+    #         while max_row - min(r_t1,r_t2)> 0:
+    #             to_add = torch.cat(
+    #                 (
+    #                     to_add,
+    #                  torch.zeros((*to_add.shape[:2],1,to_add.shape[-1]),device=to_add.device)
+    #                 ),
+    #                 -2
+    #             )
+    #             max_row -= 1
+    #         if r_t1 > r_t2:
+    #             t2 = to_add
+    #         else:
+    #             t1 = to_add
 
-        if max_col is not None:
-            to_add = t2 if max_col == c_t1 else t1
-            while max_col - min(c_t1,c_t2) > 0:
-                to_add = torch.cat(
-                    (
-                        to_add,
-                     torch.zeros((*to_add.shape[:3],1),device=to_add.device)
-                    ),
-                    -2
-                )
-                max_col -=1
+    #     if max_col is not None:
+    #         to_add = t2 if max_col == c_t1 else t1
+    #         while max_col - min(c_t1,c_t2) > 0:
+    #             to_add = torch.cat(
+    #                 (
+    #                     to_add,
+    #                  torch.zeros((*to_add.shape[:3],1),device=to_add.device)
+    #                 ),
+    #                 -2
+    #             )
+    #             max_col -=1
 
-            if c_t1 > c_t2:
-                t2 = to_add
-            else:
-                t1 = to_add
-        return t1,t2
+    #         if c_t1 > c_t2:
+    #             t2 = to_add
+    #         else:
+    #             t1 = to_add
+    #     return t1,t2
 
 
     @classmethod
